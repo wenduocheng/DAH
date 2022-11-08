33f2c75abfb52b6abf017cabf166c9499a473a79
wenduocheng <76949478+wenduocheng@users.noreply.github.com>
2022-11-07 20:58:23 -0500

    Update wc_assembler.go

--- src/GenomeAssembler/wc_assembler.go
+++ src/GenomeAssembler/wc_assembler.go
@@ -39,6 +39,7 @@ func main() {
 // String Composition Problem: Generate the k-mer composition of a string.
 // Input: An integer k and a string Text.
 // Output: Compositionk(Text), where the k-mers are arranged in lexicographic order.
+// Wenduo
 func StringComposition(k int, text string) []string {
 	//count how many times each kmer occurred in this sequence
 	kmers := KmerHash(k, text)
@@ -54,6 +55,7 @@ func StringComposition(k int, text string) []string {
 // KmerHash
 // Input: An integer k and a string Text.
 // Output: a map of kmer and frequency
+// Wenduo
 func KmerHash(k int, text string) map[string]int {
 	//count how many times each kmer occurred in this sequence
 	kmers := make(map[string]int)
@@ -73,6 +75,7 @@ func KmerHash(k int, text string) map[string]int {
 // String Spelled by a Genome Path Problem. Reconstruct a string from its genome path.
 // Input: A sequence path of k-mers Pattern1, … ,Patternn such that the last k - 1 symbols of Patterni are equal to the first k-1 symbols of Patterni+1 for 1 ≤ i ≤ n-1.
 // Output: A string Text of length k+n-1 such that the i-th k-mer in Text is equal to Patterni (for 1 ≤ i ≤ n).
+// Wenduo
 func ReconstructStringFromGenomePath(sequencePath []string) string {
 	genome := sequencePath[0]
 	k := len(sequencePath[0])
@@ -86,6 +89,7 @@ func ReconstructStringFromGenomePath(sequencePath []string) string {
 // Construct the overlap graph of a collection of k-mers.
 // Input: A collection Patterns of k-mers.
 // Output: The overlap graph Overlap(Patterns), in the form of an adjacency matix/list.
+// Wenduo
 func OverlapGraph(kmersCollection []string) [][]int {
 	if len(kmersCollection) <= 1 {
 		panic("Erros: too few kmers")
@@ -106,6 +110,7 @@ func OverlapGraph(kmersCollection []string) [][]int {
 // Suffix
 // Given: A string respresenting a kmer
 // Return: A string corresponding to the suffix of the kmer
+// Wenduo
 func Suffix(kmer string) string {
 	if len(kmer) <= 1 {
 		panic("Error: kmer size too small")
@@ -116,6 +121,7 @@ func Suffix(kmer string) string {
 // Prefix
 // Given: A string respresenting a kmer
 // Return: A string corresponding to the prefix of the kmer
+// Wenduo
 func Prefix(kmer string) string {
 	if len(kmer) <= 1 {
 		panic("Error: kmer size too small")
@@ -123,30 +129,12 @@ func Prefix(kmer string) string {
 	return kmer[0:(len(kmer) - 1)]
 }
 
-type Graph struct {
-	nodes map[string]*Node //k-1 mer
-	edges map[string]*Edge //kmer
-
-}
-
-type Node struct {
-	label     string
-	inDegree  int
-	outDegree int
-	children  []*Node
-}
-
-type Edge struct {
-	label  string
-	from   *Node
-	to     *Node
-	weight int
-}
 
 // De Bruijn Graph from a String Problem
 // Construct the de Bruijn graph of a string.
 // Input: An integer k and a string Text.
 // Output: DeBruijn_k(Tejxt), in the form of an adjacency matrix/list.
+// Wenduo; Lilin
 func DeBruijnGraph(k int, text string) map[string][]string {
 	kmerComposition := StringComposition(k, text)
 
@@ -164,6 +152,7 @@ func DeBruijnGraph(k int, text string) map[string][]string {
 	}
 	return adjacencyList
 }
+
 func EulerianCycle(graph Graph) []string {
 	//find the start node
 	var start *Node
@@ -235,7 +224,7 @@ func Sort(reads []string) []string {
 	return sortList
 }
 
-//apply divide and conquer
+// apply divide and conquer
 func DivideConquer(numlist []int) []int {
 	if len(numlist) == 1 || len(numlist) == 0 {
 		return numlist
@@ -267,6 +256,7 @@ func DivideConquer(numlist []int) []int {
 // Construct the de Bruijn graph of a string.
 // Input: An integer k and a string Text.
 // Output: a graph
+// Wenduo; Lilin
 func DeBruijnGraph2(k int, text string) Graph {
 	var dbgraph Graph
 	dbnodes := make(map[string]*Node)
@@ -313,5 +303,151 @@ func DeBruijnGraph2(k int, text string) Graph {
 	}
 	dbgraph.nodes = dbnodes
 	dbgraph.edges = dbedges
+	dbgraph.root = dbgraph.nodes[kmerComposition[0]]
 	return dbgraph
+}
+
+
+// GenerateSequence randomly genertes a sequence composed of A/T/C/G
+// Wenduo
+func GenerateSequence(length int) string {
+	nucleotides := []string{"A", "T", "C", "G"}
+	sequence := ""
+	for i := 0; i < length; i++ {
+		index := rand.Intn(4)
+		sequence += nucleotides[index]
+	}
+	return sequence
+}
+
+// RandomMutate randomly mutates a slice of kmers
+// Wenduo
+func RandomMutate(kmers []string, numMutations int) []string {
+	nucleotides := []string{"A", "T", "C", "G"}
+	mutatedKmers := make([]string, len(kmers))
+	// Copy kmers
+	for index, val := range kmers {
+		mutatedKmers[index] = val
+	}
+	for i := 0; i < numMutations; i++ {
+		kmerIndex := rand.Intn(len(kmers))        // which kmer to be mutated
+		positionIndex := rand.Intn(len(kmers[0])) // which position of a kmer to be mutated
+		nucleotideIndex := rand.Intn(4)           // randomly choose A/T/C/G
+		mutatedKmers[kmerIndex] = kmers[kmerIndex][0:positionIndex] + nucleotides[nucleotideIndex] + kmers[kmerIndex][positionIndex+1:]
+	}
+	return mutatedKmers
+}
+
+// Shuffle shuffles a slice of kmers
+// Wenduo
+func Shuffle(kmers []string) []string {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(kmers), func(i, j int) { kmers[i], kmers[j] = kmers[j], kmers[i] })
+	return kmers
+}
+
+// Delete deletes numDeletions kmers from a slice of kmers
+// Wenduo
+func Delete(kmers []string, numDeletions int, shuffle bool) []string {
+	if shuffle {
+		kmers = Shuffle(kmers)
+	}
+	for i := 0; i < numDeletions; i++ {
+		kmers = kmers[:(len(kmers) - 1)]
+	}
+	return kmers
+}
+
+// Noisify introduces noise to a set of kmers by shuffling, mutating and deleting them
+// Wenduo
+func Noisify(kmers []string, numMutations, numDeletions int) []string {
+	deletedkmers := Delete(kmers, numDeletions, true)
+	mutatedKmers := RandomMutate(deletedkmers, numMutations)
+	return mutatedKmers
+
+}
+
+// GetUniqueKmerCounts
+// Input: map of kmer to frequency
+// Output: a list of unique kmer counts (and the number of kmers with this count) present in the kmerCounts map.
+// Wenduo
+func GetUniqueKmerCounts(kmerCounts map[string]int) map[int]int {
+	kmerUniqCounts := make(map[int]int)
+
+	for _, count := range kmerCounts {
+		kmerUniqCounts[count]++
+	}
+
+	return kmerUniqCounts
+}
+
+// KmerCountSort
+// Input: kmerUniqCounts
+// Output: a sorted list of kmer count values
+// to generate distribution plot
+// Wenduo
+func kmerCountSort(kmerUniqCounts map[int]int) []int {
+	sortedCounts := make([]int, 0)
+	for count := range kmerUniqCounts {
+		sortedCounts = append(sortedCounts, count)
+	}
+	sort.Ints(sortedCounts)
+	return sortedCounts
+}
+
+// If there are two connected nodes in the graph without a divergence, merge the two nodes.
+// ChainMerging
+// Input: Graph
+// Output: Graph
+// Wenduo
+func (dbGraph Graph) ChainMerging() Graph {
+	var newGraph Graph
+
+	visited := make(map[string]bool)
+	for _, node := range dbGraph.nodes {
+		visited[node.label] = false
+	}
+	node := dbGraph.root // choose a starting node
+	kmerLength := len(node.label) + 1
+	toBeMerged := make([]*Node, 0)
+	stack := make([]*Node, 0)
+	stack = append(stack, node)
+	// run a DFS
+	for len(stack) != 0 {
+		node = stack[len(stack)-1]
+		stack = stack[:(len(stack) - 1)]
+		if len(node.children) <= 1 {
+			toBeMerged = append(toBeMerged, node)
+		} else { // have two or more children
+			// add node to newGraph.nodes
+			newGraph.nodes[node.label] = node
+			// add edge to newGraph.edges
+			for _, child := range node.children {
+				kmer := node.label + child.label[kmerLength-1:kmerLength] // kmer is node.label+last character of child.label
+				newGraph.edges[kmer] = dbGraph.edges[kmer]
+			}
+			// Merge nodes in toBeMerged and add to graph
+			// Merge(toBeMerged) // merge the nodes into one
+			// // add to new graph
+			var mergedNode Node
+			newLabel := toBeMerged[0].label
+			for i := 1; i < len(toBeMerged); i++ {
+				newLabel += toBeMerged[i].label[kmerLength-1 : kmerLength]
+			}
+			mergedNode.label = newLabel
+			mergedNode.children = toBeMerged[len(toBeMerged)].children
+			mergedNode.inDegree = toBeMerged[0].inDegree
+			mergedNode.outDegree = toBeMerged[len(toBeMerged)].outDegree
+			newGraph.nodes[mergedNode.label] = &mergedNode
+
+			toBeMerged = make([]*Node, 0)
+
+		}
+		for _, nextNode := range node.children {
+			if !visited[nextNode.label] {
+				stack = append(stack, nextNode)
+			}
+		}
+	}
+	return newGraph
 }
