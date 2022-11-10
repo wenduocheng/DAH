a563d4f85b07d2a83f0ce3fa9c7db8ec750d999d
Leo-o333 <45889981+Leo-o333@users.noreply.github.com>
2022-11-09 22:54:44 -0500

    Update main.go

--- src/GenomeAssembler/main.go
+++ src/GenomeAssembler/main.go
@@ -33,5 +33,26 @@ func main() {
 	for i := range graph.edges {
 		fmt.Println(graph.edges[i].label, graph.edges[i].from.label, graph.edges[i].to.label, graph.edges[i].weight)
 	}
+	//Example of graphing
+	//Because previous variable is conflit with package, so I change it to another one
+	graph_plot := graph
+	graph_plot := graph.New(graph.StringHash, graph.Directed())
+
+	for key := range graph_plot.nodes {
+		_ = g.AddVertex(graph_plot.nodes[key].label)
+		fmt.Println(graph_plot.nodes[key].label)
+		//cannot label edges as integer so please ignore these codes
+		// _ = g.AddEdge(dbj.edges[key].from.label, dbj.edges[key].to.label)
+		// fmt.Println(dbj.edges[key].from.label, dbj.edges[key].to.label)
+
+	}
+	for key := range graph_plot.edges {
+		_ = g.AddEdge(graph_plot.edges[key].from.label, graph_plot.edges[key].to.label)
+		fmt.Println(graph_plot.edges[key].from.label, graph_plot.edges[key].to.label)
+
+	}
+	//The file is generated as a DOT file
+	file, _ := os.Create("./mygraph.gv")
+	_ = draw.DOT(g, file)
 
 }
