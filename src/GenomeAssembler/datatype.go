fc92adeb03b0d177d8b6727e7b8d21677907e0a5
Wenduo Cheng <76949478+wenduocheng@users.noreply.github.com>
2022-11-24 00:41:06 -0500

    Update datatype.go

--- src/GenomeAssembler/datatype.go
+++ src/GenomeAssembler/datatype.go
@@ -5,12 +5,14 @@ type Graph struct {
 	nodes map[string]*Node //k-1 mer
 	edges map[string]*Edge //kmer
 }
+
 type Node struct {
 	label     string
 	inDegree  int
 	outDegree int
 	children  []*Node
 }
+
 type Edge struct {
 	label  string
 	from   *Node
