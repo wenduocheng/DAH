9e5678d75d2523f0d9c3726281e95499f21fbc49
Wenduo Cheng <76949478+wenduocheng@users.noreply.github.com>
2022-11-24 01:49:30 -0500

    Add files via upload

---
+++ src/GenomeAssembler/kmer_size_selection.go
@@ -0,0 +1,256 @@
+package main
+
+import "math"
+
+// N50 takes the list of contigs and return the shortest contig length needed to cover 50% genome,
+// describing the completeness of genome assembly
+// Input: the list of contigs
+// Output: The length of the shortest contig cover 50% genome
+// Lilin
+func N50(contigs []string) int { //contigs
+	var total_length int
+	contigs = Sort(contigs)
+	for _, contig := range contigs {
+		total_length += len(contig)
+	}
+
+	N50_length := (float64(total_length) / 2.0)
+	var currLength int
+	var N50_string string
+	for _, contig := range contigs {
+		currLength += len(contig)
+		if float64(currLength) >= N50_length {
+			N50_string = contig
+			break
+		}
+	}
+
+	return len(N50_string)
+}
+
+// Sort sorts the contigs according to their length from longest to shortest
+// Input: the list of contigues
+// Output: the list of the sorted contigs from longest to shortest
+// Lilin
+func Sort(contigs []string) []string {
+	lenHash := make(map[int][]string)
+	for _, contig := range contigs {
+		length := len(contig)
+		_, Exists := lenHash[length]
+		if Exists {
+			lenHash[length] = append(lenHash[length], contig)
+		} else {
+			lenHash[length] = []string{contig}
+		}
+	}
+	var lengthList []int
+	for key, _ := range lenHash {
+		lengthList = append(lengthList, key)
+	}
+	lengthList = DivideConquer(lengthList)
+	var sortList []string
+	for _, num := range lengthList {
+		stringList := lenHash[num]
+		sortList = append(sortList, stringList...)
+	}
+	return sortList
+}
+
+// DivideConquer sorts the list of int from largest to smallest using divide&conquer algorithm
+// Intput: a list of integer
+// Output: a sortest list of integer from largest to smallest
+// Lilin
+func DivideConquer(numlist []int) []int {
+	if len(numlist) == 1 || len(numlist) == 0 {
+		return numlist
+	}
+	i := int(len(numlist) / 2)
+	right := DivideConquer(numlist[:i])
+	left := DivideConquer(numlist[i:])
+	var combineList []int
+	for len(right) != 0 && len(left) != 0 {
+		if right[0] >= left[0] {
+			combineList = append(combineList, right[0])
+			right = right[1:]
+		} else if right[0] < left[0] {
+			combineList = append(combineList, left[0])
+			left = left[1:]
+		}
+	}
+	if len(right) > 0 {
+		combineList = append(combineList, right...)
+	}
+	if len(left) > 0 {
+		combineList = append(combineList, left...)
+	}
+	return combineList
+}
+
+// DistinctKmerCount counts the number of distinct kmer in the string
+// Input: a map of kmer and frequency
+// Output: the number of distinct kmer
+// Lilin
+func DistinctKmerCount(kmerCounts map[string]int) int {
+
+	count := len(kmerCounts)
+
+	return count
+}
+
+// KmerSizeAdjustment counts the kmer size according to the readlength and kmer and genome coverage
+// Input: the readlength, kmer coverage and genome coverage
+// Output: the estimate optimal kmer size
+// Lilin
+func KmerSizeAdjustment(readlength int, kmer_coverage, genome_coverage float64) int {
+
+	var kmer_size int
+	kmer_size = 1 + readlength - int((kmer_coverage*float64(readlength))/genome_coverage)
+
+	return kmer_size
+}
+
+// GenomeCoverage counts the genome coverage for the current kmer set
+// Input: the estimate genome length, the readlength and the reads number
+// Output: the genome coverage
+// Lilin
+func GenomeCoverage(genome_length, readlength, reads_number int) float64 {
+	var genome_coverage float64
+
+	//(A total number of reads * read length)/ (Estimated genome size)
+	genome_coverage = float64(reads_number) * float64(readlength) / float64(genome_length)
+
+	return genome_coverage
+}
+
+// KmerCoverage counts the kmer coverage for the current kmer set
+// Input: the read length, kmer size and genome coverage
+// Output: the kmer coverage
+// Lilin
+func KmerCoverage(readlength, kmersize int, genome_coverage float64) float64 {
+
+	var kmer_coverage float64
+	base_coverage := genome_coverage
+	//𝐶𝑘=𝐶⋅(𝑅−𝐾+1)/𝑅
+	kmer_coverage = base_coverage * float64(readlength-kmersize+1) / float64(readlength)
+
+	return kmer_coverage
+}
+
+// KmerSizeSet takes the readlength and generate the set of interger for range over to pick the optimal kmer size form this set
+// Input: the readlength
+// Output: the generated kmer size set
+// Lilin
+func KmerSizeSet(readlength int) []int {
+	var size_set []int
+	//find the prome number less or equal to the readlength
+	size_set = FindPrime(readlength)
+	return size_set
+}
+
+// FindPrime takes in the value and returns the list of prime number smaller than that value
+// in this case, although 1 is not prime number, we also take it into consideration
+// Input: the value
+// Output: the int list contains the prime number smaller than the value
+// Lilin
+func FindPrime(n int) []int {
+	var prime_list []int
+	//special case even 1 is not prime
+	if n == 1 {
+		prime_list = append(prime_list, 1)
+		return prime_list
+	}
+	if n == 2 {
+		prime_list = append(prime_list, 1)
+		prime_list = append(prime_list, 2)
+		return prime_list
+	}
+
+	var number_list []int
+	for i := 0; i <= n; i++ {
+		number_list = append(number_list, i)
+	}
+
+	var if_prime_list []bool
+	if_prime_list = make([]bool, n+1)
+	//1 and 2 are prime
+	if_prime_list[0] = false
+	if_prime_list[1] = true
+
+	for i := 2; i <= n; i++ {
+		if_prime_list[i] = true
+	}
+	sqrtn := int(math.Sqrt(float64(n))) + 1
+	//cross out multiple of the prime numebr
+	for i := 2; i < sqrtn; i++ {
+		if if_prime_list[i] {
+			for j := i * i; j <= n; j += i {
+				if_prime_list[j] = false
+			}
+		}
+	}
+
+	// if the nth position of the if_prime_list is true,
+	// the number at nth position of number_list is prime
+	for n := range if_prime_list {
+		if if_prime_list[n] {
+			prime_list = append(prime_list, number_list[n])
+		}
+	}
+
+	return prime_list
+}
+
+// OptimalKmerSize gives the optiaml kmersize for the later analysis
+// Input: genome, list of reads
+// Output: optimal kmer size
+// Lilin
+func OptimalKmerSize(reads []string) int {
+
+	var optimalk int
+
+	readlength := len(reads[0])
+	//method1: find the k gives most number of distinct kmers
+	k_size_set := KmerSizeSet(readlength)
+	max_distinct_kmer_count := 0
+	var optimalk1 int
+	for i := range k_size_set {
+		k := k_size_set[i]
+
+		var kmerCounts map[string]int
+		for _, read := range reads {
+			kmerCounts = KmerHash0(k, read, kmerCounts)
+		}
+		distinct_kmer_count := DistinctKmerCount(kmerCounts)
+		if distinct_kmer_count > max_distinct_kmer_count {
+			max_distinct_kmer_count = distinct_kmer_count
+			optimalk1 = k
+		}
+	}
+
+	//methods improve result of first method from genome and kmer coverage
+	var kmer_coverage float64
+	var genome_coverage float64
+	var readsnumber int
+	var genome_length int
+
+	genome_length = GenomeSizeEstimate(reads)
+	readsnumber = len(reads)
+	genome_coverage = GenomeCoverage(genome_length, readlength, readsnumber)
+	kmer_coverage = KmerCoverage(readlength, optimalk1, genome_coverage)
+
+	optimalk = KmerSizeAdjustment(readlength, kmer_coverage, genome_coverage)
+
+	return optimalk
+
+}
+
+// GenomeSizeEstimate estimates the size of the genome
+// Input: the reads set
+// Output: the size of genome
+// To be changed
+func GenomeSizeEstimate(reads []string) int {
+	var estimate_size int
+	//our reads is 10X
+	estimate_size = len(reads) / 10
+	return estimate_size
+}
---
+++ src/GenomeAssembler/reads_generation.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+// GenerateSequence randomly genertes a sequence composed of A/T/C/G
+// Wenduo
+func GenerateSequence(length int) string {
+	if length < 0 {
+		panic("Error: Negative integer is given. Please give a nonnegative integer.")
+	}
+	nucleotides := []string{"A", "T", "C", "G"}
+	sequence := ""
+	for i := 0; i < length; i++ {
+		index := rand.Intn(4)
+		sequence += nucleotides[index]
+	}
+	return sequence
+}
+
+// GenerateReadsNaive will generate a list of reads.
+// Input: an integer corresponding to read length, an integer corresponding to the number of copies of each read to be generated, sequence string
+// Output: a list of strings corresponding to generated reads
+// Wenduo
+func GenerateReadsNaive(readLength int, numberOfCopies int, sequence string) []string {
+	if readLength > len(sequence) {
+		panic("Error: The read length has to be less than the sequence length.")
+	}
+
+	reads := make([]string, 0)
+	for i := 0; i <= len(sequence)-readLength; i++ {
+		reads = append(reads, sequence[i:i+readLength])
+	}
+
+	reads_copy := make([]string, len(reads))
+	for j := 0; j < len(reads); j++ {
+		reads_copy[j] = reads[j]
+	}
+
+	for k := 0; k < numberOfCopies-1; k++ {
+		reads = append(reads, reads_copy...)
+	}
+
+	return reads
+}
+
+// GenerateReadsNaive will generate random reads by given read length. It will generte a list of strings with given readlength and different num of reads
+// input: int,int
+// tianyue
+//
+//output:reads []string
+func GenerateReadsNaive0(readlength int, differentReads int) []string {
+	rand.Seed(time.Now().UnixNano())
+	var temp []string
+	var LetterRunes = []rune("ATCG")
+	Sequence := make([]rune, readlength)
+
+	for j := 0; j < differentReads; j++ {
+		for i := range Sequence {
+			Sequence[i] = LetterRunes[rand.Intn(len(LetterRunes))]
+
+		}
+		//sequence will transfer rune type into string
+		sequence := string(Sequence)
+		temp = append(temp, sequence)
+	}
+	var ListSequence []string
+	ListSequence = CopyReads(temp, 10)
+
+	return ListSequence
+}
+
+// Copy each reads by given number of times
+// input []string as sequence
+// output []string reads with repeats
+// tianyue
+func CopyReads(ListSequence []string, times int) []string {
+	for i := 0; i < times; i++ {
+		ListSequence = append(ListSequence, ListSequence...)
+	}
+	return ListSequence
+}
+
+// Generate read seqs with the num pf each distinct read is normaly distrubuted
+// input int int
+// output []string as read seqs
+// tianyue
+func GenerateReadsNorm0(readlength, differentreads int) []string {
+	var temp []string
+	var LetterRunes = []rune("ATCG")
+	// var NormalDist []int
+	Sequence := make([]rune, readlength)
+
+	for j := 0; j < differentreads; j++ {
+		for i := range Sequence {
+			Sequence[i] = LetterRunes[rand.Intn(len(LetterRunes))]
+
+		}
+		//sequence will transfer rune type into string
+		sequence := string(Sequence)
+		temp = append(temp, sequence)
+	}
+
+	//Generate a list number with Random Distribution
+	var b []int
+	for i := 0; i < len(temp); i++ {
+		b = append(b, int(rand.NormFloat64()*5+10))
+	}
+	var SeqReads []string
+	for j := 0; j < len(temp); j++ {
+		for i := 0; i < len(b); i++ {
+
+			SeqReads = append(SeqReads, temp[j])
+		}
+	}
+	return SeqReads
+}
+
+// RandomMutate randomly mutates a slice of kmers
+// It is possible that even after mutation, genome is still the same.
+// Wenduo
+func RandomMutate(kmers []string, numMutations int) []string {
+	nucleotides := []string{"A", "T", "C", "G"}
+	mutatedKmers := make([]string, len(kmers))
+	// Copy kmers
+	for index, val := range kmers {
+		mutatedKmers[index] = val
+	}
+	for i := 0; i < numMutations; i++ {
+		kmerIndex := rand.Intn(len(kmers))        // which kmer to be mutated
+		positionIndex := rand.Intn(len(kmers[0])) // which position of a kmer to be mutated
+		nucleotideIndex := rand.Intn(4)           // randomly choose A/T/C/G
+		mutatedKmers[kmerIndex] = kmers[kmerIndex][0:positionIndex] + nucleotides[nucleotideIndex] + kmers[kmerIndex][positionIndex+1:]
+	}
+	return mutatedKmers
+}
+
+// Shuffle shuffles a slice of kmers
+// Wenduo
+func Shuffle(kmers []string) []string {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(kmers), func(i, j int) { kmers[i], kmers[j] = kmers[j], kmers[i] })
+	return kmers
+}
+
+// Delete deletes numDeletions kmers from a slice of kmers
+// Wenduo
+func Delete(kmers []string, numDeletions int, shuffle bool) []string {
+	if shuffle {
+		kmers = Shuffle(kmers)
+	}
+	for i := 0; i < numDeletions; i++ {
+		kmers = kmers[:(len(kmers) - 1)]
+	}
+	return kmers
+}
+
+// Noisify introduces noise to a set of kmers by shuffling, mutating and deleting them
+// Wenduo
+func Noisify(kmers []string, numMutations, numDeletions int) []string {
+	deletedkmers := Delete(kmers, numDeletions, true)
+	mutatedKmers := RandomMutate(deletedkmers, numMutations)
+	return mutatedKmers
+}
