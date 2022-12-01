701e3e5e7bceee6624545639b72cf67df6a1beed
Wenduo Cheng <76949478+wenduocheng@users.noreply.github.com>
2022-11-30 23:25:20 -0500

    Update main.go

--- src/GenomeAssembler/main.go
+++ src/GenomeAssembler/main.go
@@ -4,61 +4,49 @@ import "fmt"
 
 func main() {
 
-	pseudoSequence := GenerateSequence(20)
-	fmt.Println(pseudoSequence)
-	readLength := 10
-	numberOfCopies := 3
-	reads := GenerateReadsNaive(readLength, numberOfCopies, pseudoSequence)
-	fmt.Println(reads)
+		viralSequence := "TTGTAGATCTGTTCTCTAAACGAACTTTAAAATCTGTGTGGCTGTCACTCGGCTGCATGCTTAGTGCACTCACGCAGTATAATTAATAACTAATTACTGTCGTTGACAGGACACGAGTAACTCGTCTATCTTCTGCAGGCTGCTTACGGTTTCGTCCGTGTTGCAGCCGATCATCAGCACATCTAGGTTTTGTCCGGGTGTGACCGAAAGGTAAGATGGAGAGCCTTGTCCCTGGTTTCAACGAGAAAACACACGTCCAACTCAGTTTGCCTGTTTTACAGGTTCGCGACGTGCTCGTACGTGGCTTTGGAGACTCCGTGGAGGAGGTCTTATCAGAGGCACGTCAACATCTTAAAGATGGCACTTGTGGCTTAGTAGAAGTTGAAAAAGGCGTTTTGCCTCAACTTGAACAGCCCTATG"
+	readLength := 150
+	readCounts := 3000
+	reads := GenerateReads(readLength, readCounts, viralSequence)
+	// fmt.Println(reads)
 
-	numMutations := 5
-	numDeletions := 1
-	readsWithNoise := Noisify(reads, numMutations, numDeletions)
-	fmt.Println(readsWithNoise)
-	
-	/////////////////////////////////
-	kmerLength := 5
-	kmerChoice := "default"
-	
-	kmermin := 3
-	kmermax := 10
-	if kmerChoice == "default" {
-		kmerLength = OptimalKmerSize(reads, numberOfCopies)
-	} else {
-		kmerLength = OptimalKmerSizeWithRange(kmermin, kmermax, reads)
-	}
-	fmt.Println(kmerLength)
-	/////////////////////////////////
-	
-	
+	kmerLength := 8
 
-	graph := DeBruijnGraph(kmerLength, reads)
-	fmt.Println("DeBrujinGraph was created")
-	for i := range graph.nodes {
-		fmt.Println("Node: ", graph.nodes[i].label, "Indegree: ", graph.nodes[i].inDegree, "Outdegree: ", graph.nodes[i].outDegree, "Children:")
-		for j := range graph.nodes[i].children {
-			fmt.Println(graph.nodes[i].children[j].label)
-		}
-	}
-	for i := range graph.edges {
-		fmt.Println(graph.edges[i].label, graph.edges[i].from.label, graph.edges[i].to.label, graph.edges[i].weight)
-	}
+	// graph := DeBruijnGraph(kmerLength, reads)
+	// fmt.Println("DeBrujinGraph was created")
+	// for i := range graph.nodes {
+	// 	fmt.Println("Node: ", graph.nodes[i].label, "Indegree: ", graph.nodes[i].inDegree, "Outdegree: ", graph.nodes[i].outDegree, "Children:")
+	// 	for j := range graph.nodes[i].children {
+	// 		fmt.Println(graph.nodes[i].children[j].label)
+	// 	}
+	// 	fmt.Println("Parents: ")
+	// 	for j := range graph.nodes[i].parents {
+	// 		fmt.Println(graph.nodes[i].parents[j].label)
+	// 	}
+	// }
+	// for i := range graph.edges {
+	// 	fmt.Println(graph.edges[i].label, graph.edges[i].from.label, graph.edges[i].to.label, graph.edges[i].weight)
+	// }
+	//
+	// fmt.Println(graph.root)
 
-	fmt.Println(graph.root)
+	// mergedGraph := graph.ChainMerging()
+	// fmt.Println("Chain merging was performed!")
+	// for i := range mergedGraph.nodes {
+	// 	fmt.Println("Node: ", mergedGraph.nodes[i].label, "Indegree: ", mergedGraph.nodes[i].inDegree, "Outdegree: ", mergedGraph.nodes[i].outDegree, "Children:")
+	// 	for j := range mergedGraph.nodes[i].children {
+	// 		fmt.Println(mergedGraph.nodes[i].children[j].label)
+	// 	}
+	// 	fmt.Println("Parents: ")
+	// 	for j := range mergedGraph.nodes[i].parents {
+	// 		fmt.Println(mergedGraph.nodes[i].parents[j].label)
+	// 	}
+	// }
+	// for i := range mergedGraph.edges {
+	// 	fmt.Println(mergedGraph.edges[i].label, mergedGraph.edges[i].from.label, mergedGraph.edges[i].to.label, mergedGraph.edges[i].weight)
+	// }
 
-	mergedGraph := graph.ChainMerging()
-	fmt.Println("Chain merging was performed!")
-	for i := range mergedGraph.nodes {
-		fmt.Println(mergedGraph.nodes[i].label, ":")
-		for j := range mergedGraph.nodes[i].children {
-			fmt.Println(mergedGraph.nodes[i].children[j].label)
-		}
-	}
-	for i := range mergedGraph.edges {
-		fmt.Println(mergedGraph.edges[i].label, mergedGraph.edges[i].from.label, mergedGraph.edges[i].to.label, mergedGraph.edges[i].weight)
-	}
-
-	fmt.Println(mergedGraph.root)
+	// fmt.Println(mergedGraph.root)
 
 	contigs := DenovoAssembler(reads, kmerLength)
 
