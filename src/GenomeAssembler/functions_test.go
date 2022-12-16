2ad5f05779ae77718b44cfc13eccd216b6a133c4
Lilin-Huang <113716424+Lilin-Huang@users.noreply.github.com>
2022-12-15 20:34:10 -0500

    Lilin Test function update

--- src/GenomeAssembler/functions_test.go
+++ src/GenomeAssembler/functions_test.go
@@ -206,90 +206,28 @@ func TestN50(t *testing.T) {
 //Lilin
 func TestEulerianPath(t *testing.T) {
 	//base case
-	text1 := "AAGATT"
-	graph1 := DeBruijnGraph2(4, text1)
+	var text1 []string
+	text1 = append(text1, "AAGATT")
+	graph1 := DeBruijnGraph(4, text1)
 	path1 := EulerianPath(graph1)
-	genome1 := ReconstructStringFromGenomePath(path1)
-	if genome1 != text1 {
+	genome1 := ReconstructStringFromGenomePath(path1[0], 4)
+	if genome1 != text1[0] {
 		t.Errorf("EulerianPath fails at base case")
 	}
 	//genome length = kmer length
-	text2 := "AGCT"
-	graph2 := DeBruijnGraph2(4, text2)
+	var text2 []string
+	text2 = append(text2, "AGCT")
+	graph2 := DeBruijnGraph(4, text2)
 	path2 := EulerianPath(graph2)
-	genome2 := ReconstructStringFromGenomePath(path2)
-	if genome2 != text2 {
+	genome2 := ReconstructStringFromGenomePath(path2[0], 4)
+	if genome2 != text2[0] {
 		t.Errorf("EulerianPath fails at kmer length = genome length")
-	}
-
-	//check no Eulerian path case
-	text3 := "AAGATT"
-	graph3 := DeBruijnGraph2(4, text3)
-	graph3.nodes["AGA"].children = graph3.nodes["AGA"].children[:len(graph3.nodes["AGA"].children)-1]
-	graph3.nodes["AGA"].outDegree = graph3.nodes["AGA"].outDegree - 1
-	path3 := EulerianPath(graph3)
-	if len(path3) != 0 {
-		t.Errorf("EulerianPath fails at checking no Eulerian path")
 	}
 
 	fmt.Println("EulerianPath all tests passed.")
 
-}//Lilin
-func TestFindPrime(t *testing.T) {
-	a := 20
-	a_prime_list := []int{1, 2, 3, 5, 7, 11, 13, 17, 19}
-	a_findprime := FindPrime(a)
-	for i, _ := range a_prime_list {
-		if a_prime_list[i] != a_findprime[i] {
-			t.Errorf("FindPrime function is wrong!")
-		}
-	}
-
-	b := 1
-	b_prime_list := []int{1}
-	b_findprime := FindPrime(b)
-	for i, _ := range b_prime_list {
-		if b_prime_list[i] != b_findprime[i] {
-			t.Errorf("FindPrime function is wrong!")
-		}
-	}
-	fmt.Println("FindPrime passes all tests.")
 }
 
-//Lilin
-func TestDistinctKmerCount(t *testing.T) {
-	text1 := "AAGAAGAAA"
-	expectedDisCount1 := 4
-	kmerCounts1 := KmerHash(3, text1)
-	realDisCount1 := DistinctKmerCount(kmerCounts1)
-	if expectedDisCount1 != realDisCount1 {
-		t.Errorf("DistinctKmerCount is wrong!")
-	}
-
-	text2 := "AGTCGCTA"
-	expectedDisCount2 := 5
-	kmerCounts2 := KmerHash(4, text2)
-	realDisCount2 := DistinctKmerCount(kmerCounts2)
-	if expectedDisCount2 != realDisCount2 {
-		t.Errorf("DistinctKmerCount is wrong!")
-	}
-	fmt.Println("DistinctKmerCount passes all tests.")
-}
-
-//Lilin
-func TestGenomeCoverage(t *testing.T) {
-	genome := "AGTCAGTCAGTCAGTCAGTC" //20
-	readlength := 5
-	reads_number := 10
-
-	genome_coverage := GenomeCoverage(genome, readlength, reads_number)
-
-	if genome_coverage != 2.5 {
-		t.Errorf("GenomeCoverage is wrong!")
-	}
-
-	fmt.Println("GenomeCoverage passes all tests.")
-}
 //Lilin
 func TestFindPrime(t *testing.T) {
 	a := 20
@@ -314,17 +252,20 @@ func TestFindPrime(t *testing.T) {
 
 //Lilin
 func TestDistinctKmerCount(t *testing.T) {
-	text1 := "AAGAAGAAA"
+
+	var text1 []string
+	text1 = append(text1, "AAGAAGAAA")
 	expectedDisCount1 := 4
-	kmerCounts1 := KmerHash(3, text1)
+	kmerCounts1 := KmerHashFromReads(3, text1)
 	realDisCount1 := DistinctKmerCount(kmerCounts1)
 	if expectedDisCount1 != realDisCount1 {
 		t.Errorf("DistinctKmerCount is wrong!")
 	}
 
-	text2 := "AGTCGCTA"
+	var text2 []string
+	text2 = append(text2, "AGTCGCTA")
 	expectedDisCount2 := 5
-	kmerCounts2 := KmerHash(4, text2)
+	kmerCounts2 := KmerHashFromReads(4, text2)
 	realDisCount2 := DistinctKmerCount(kmerCounts2)
 	if expectedDisCount2 != realDisCount2 {
 		t.Errorf("DistinctKmerCount is wrong!")
@@ -337,8 +278,8 @@ func TestGenomeCoverage(t *testing.T) {
 	genome := "AGTCAGTCAGTCAGTCAGTC" //20
 	readlength := 5
 	reads_number := 10
-
-	genome_coverage := GenomeCoverage(genome, readlength, reads_number)
+	genome_length := len(genome)
+	genome_coverage := GenomeCoverage(genome_length, readlength, reads_number)
 
 	if genome_coverage != 2.5 {
 		t.Errorf("GenomeCoverage is wrong!")
