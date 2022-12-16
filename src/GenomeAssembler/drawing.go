131cf38ebe2ea387b9d689d11e9b10b25c919e82
Leo-o333 <45889981+Leo-o333@users.noreply.github.com>
2022-12-16 14:47:22 -0500

    Update drawing.go

--- src/GenomeAssembler/drawing.go
+++ src/GenomeAssembler/drawing.go
@@ -3,11 +3,15 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"image/color"
 	"os"
 	"strconv"
 
 	"github.com/dominikbraun/graph"
 	"github.com/dominikbraun/graph/draw"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
 )
 
 // func main() {
@@ -21,6 +25,71 @@ import (
 // Input: UniqueKmerCount []int
 // Output: Scatter plot
 //tianyue
+
+// Draw graph based on given De Bruijn Graph
+// Input: De Bruijn Graph (datatype as nodes map[string]*Node //k-1 mer; edges map[string]*Edge)
+// Output: De Bruijn Graph where vertices are k-1 mers and edges are k-mers
+//tianyue
+func (deBruijn Graph) DrawDeBruijnGraph() {
+	deBruijn_plot := graph.New(graph.StringHash, graph.Directed())
+
+	for key := range deBruijn.nodes {
+		_ = deBruijn_plot.AddVertex(deBruijn.nodes[key].label)
+		fmt.Println(deBruijn.nodes[key].label)
+		// _ = g.AddEdge(dbj.edges[key].from.label, dbj.edges[key].to.label)
+		// fmt.Println(dbj.edges[key].from.label, dbj.edges[key].to.label)
+
+	}
+	for key := range deBruijn.edges {
+		_ = deBruijn_plot.AddEdge(deBruijn.edges[key].from.label, deBruijn.edges[key].to.label)
+		fmt.Println(deBruijn.edges[key].from.label, deBruijn.edges[key].to.label)
+
+	}
+	file, _ := os.Create("./mygraph.gv")
+	_ = draw.DOT(deBruijn_plot, file)
+
+}
+
+// In our graph construct, we have Node as a map where keys represent the k-1 mers where we are connecting to the next one, and generate a gfa file that can be read by bandage
+// Input: de Bruijn Graph
+// output: GFA file
+// tianyue
+func SaveGraphToGFA(graph Graph, prefix string) {
+	fmt.Println("SaveGraphToGFA")
+	//create a reference map
+	reference := make(map[string]int)
+	counter := 0
+	for key := range graph.nodes {
+		counter += 1
+		reference[key] = counter
+	}
+
+	f, err := os.Create(prefix + "output.gfa")
+	if err != nil {
+		fmt.Println(err)
+	}
+	w := bufio.NewWriter(f)
+	w.WriteString("H" + "\t" + "\n")
+
+	//write the segment part
+	for key := range graph.nodes {
+		idx := reference[key]
+
+		w.WriteString("S" + "\t" + strconv.Itoa(idx) + "\t" + key + "\n")
+
+		// w.WriteString(key + "\n")
+	}
+	//write the linkage part
+	// counter_2 :=0
+	for _, val := range graph.edges {
+		from := reference[val.from.label]
+		to := reference[val.to.label]
+		w.WriteString("L" + "\t" + strconv.Itoa(from) + "\t" + "-" + "\t" + strconv.Itoa(to) + "\t" + "-" + "\t" + "5M" + "\n")
+
+	}
+	w.Flush()
+
+}
 func DrawKmerScatter(uniqueKmerCounts map[int]int, title, xlabel, ylabel string) {
 	//due to the package is using interface, I need to check more about this
 
@@ -72,70 +141,5 @@ func DrawKmerScatter(uniqueKmerCounts map[int]int, title, xlabel, ylabel string)
 	// line.LineStyle.Color = color.RGBA{B: 255, A: 255}
 	plot.Add(sca)
 	plot.Save(200, 200, "scatter_trial.png")
-
-}
-
-// Draw graph based on given De Bruijn Graph
-// Input: De Bruijn Graph (datatype as nodes map[string]*Node //k-1 mer; edges map[string]*Edge)
-// Output: De Bruijn Graph where vertices are k-1 mers and edges are k-mers
-//tianyue
-func (deBruijn Graph) DrawDeBruijnGraph() {
-	deBruijn_plot := graph.New(graph.StringHash, graph.Directed())
-
-	for key := range deBruijn.nodes {
-		_ = deBruijn_plot.AddVertex(deBruijn.nodes[key].label)
-		fmt.Println(deBruijn.nodes[key].label)
-		// _ = g.AddEdge(dbj.edges[key].from.label, dbj.edges[key].to.label)
-		// fmt.Println(dbj.edges[key].from.label, dbj.edges[key].to.label)
-
-	}
-	for key := range deBruijn.edges {
-		_ = deBruijn_plot.AddEdge(deBruijn.edges[key].from.label, deBruijn.edges[key].to.label)
-		fmt.Println(deBruijn.edges[key].from.label, deBruijn.edges[key].to.label)
-
-	}
-	file, _ := os.Create("./mygraph.gv")
-	_ = draw.DOT(deBruijn_plot, file)
-
-}
-
-// In our graph construct, we have Node as a map where keys represent the k-1 mers where we are connecting to the next one, and generate a gfa file that can be read by bandage
-// Input: de Bruijn Graph
-// output: GFA file
-// tianyue
-func SaveGraphToGFA(graph Graph) {
-	fmt.Println("SaveGraphToGFA")
-	//create a reference map
-	reference := make(map[string]int)
-	counter := 0
-	for key := range graph.nodes {
-		counter += 1
-		reference[key] = counter
-	}
-
-	f, err := os.Create("output.gfa")
-	if err != nil {
-		fmt.Println(err)
-	}
-	w := bufio.NewWriter(f)
-	w.WriteString("H" + "\t" + "\n")
-
-	//write the segment part
-	for key := range graph.nodes {
-		idx := reference[key]
-
-		w.WriteString("S" + "\t" + strconv.Itoa(idx) + "\t" + key + "\n")
-
-		// w.WriteString(key + "\n")
-	}
-	//write the linkage part
-	// counter_2 :=0
-	for _, val := range graph.edges {
-		from := reference[val.from.label]
-		to := reference[val.to.label]
-		w.WriteString("L" + "\t" + strconv.Itoa(from) + "\t" + "+" + "\t" + strconv.Itoa(to) + "\t" + "+" + "\t" + "5M" + "\n")
-
-	}
-	w.Flush()
 
 }
